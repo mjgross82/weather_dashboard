b157620a8a6342f3b72bed2f6f511319999744ad
Michael Gross <mjgross@protonmail.com>
2020-07-02 16:55:58 -0600

    Added function to populate forecast data.

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
     var APIKey = "f7298ae521ac0931bc7a0c7fed9d21ad";
     var city = "Salt Lake City";
     var todayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    var futureURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
     getWeather();
 
     // Function to call the API using the city named in var City
@@ -17,13 +16,32 @@ $(document).ready(function() {
             console.log(response);
             // Convert temperature Kelvin to Fahrenheit
             var degrees = Math.round((response.main.temp - 273.15) * 1.8 +32);
-            console.log(degrees);
+            // Save latitude and longitude in variables to call in forecast API
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var futureURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=" + APIKey;
             // Populate today's forecast
-            var city = response.name;
+            icon = response.weather[0].icon;
+            iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+            $("#todayLocale").text(response.name + " DATE");
             $("#todayTemp").text(degrees + "° F");
-            $("#todayLocale").text(city + " DATE");
             $("#todayHumid").text("Humidity: " + response.main.humidity + "%");
             $("#todayWind").text("Wind Speed: " + response.wind.speed + " MPH");
+            // Function to call the API for forecast data and populate into HTML
+            $.ajax({
+                url: futureURL,
+                method: "GET"
+              }).then(function(response) {
+                console.log(response);
+                $("#todayUV").text("UV Index: " + response.current.uvi);
+                var daily = response.daily;
+                var forecast = [0, 1, 2, 3, 4];
+                $.each(forecast, function(value) {
+                    var temp = Math.round((daily[value].temp.day - 273.15) * 1.80 + 32);
+                    $("#" + (value + 1) + "temp").text(temp + "° F");
+                    $("#" + (value + 1) + "humid").text("Humidity: " + daily[value].humidity + "%")
+                });
+            });
         });
     };    
 });
